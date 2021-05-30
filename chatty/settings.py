06cce2dfbf6f76779064cdeac0d9b69e3463df19
Abdiev003 <aliabdiyev000@gmail.com>
2021-05-30 13:30:34 +0300

    deployment

--- chatty/settings.py
+++ chatty/settings.py
@@ -132,9 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = [
-        BASE_DIR / "static",
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # BASE_DIR / "static",
+# if DEBUG:
+#     STATICFILES_DIRS = [
+#         BASE_DIR / "static",
+#     ]
+# else:
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # BASE_DIR / "static",
